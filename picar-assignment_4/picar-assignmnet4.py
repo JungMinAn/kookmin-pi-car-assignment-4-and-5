d15a1e8698a5ec0a4ac1b70ea1e7dd2bdb93fbd2
minwook <minwook0106@gmail.com>
2017-11-06 14:38:17 +0900

    init def part

--- picar-assignment_4/picar-assignmnet4.py
+++ picar-assignment_4/picar-assignmnet4.py
@@ -1,26 +1,197 @@
 #########################################################################
-### Date: 2017/10/13
-### file name: trackingModule.py
-### Purpose: this code has been generated for the five-way tracking sensor
-###         to perform the decision of direction
-###
+# Date: 2017/11/06
+# file name: picar-assignment4.py
 #########################################################################
 
-# =======================================================================
-# import GPIO library and time module
-# =======================================================================
 import RPi.GPIO as GPIO
-from time import sleep
+import time
 
-# =======================================================================
-#  set GPIO warnings as false
-# =======================================================================
 GPIO.setwarnings(False)
-
-# =======================================================================
-# set up GPIO mode as BOARD
-# =======================================================================
 GPIO.setmode(GPIO.BOARD)
+
+pwm_setup()
+
+trig = 33
+echo = 31
+
+GPIO.setup(trig, GPIO.OUT)
+GPIO.setup(echo, GPIO.IN)
+
+
+def getDistance():
+    GPIO.output(trig, False)
+    time.sleep(0.5)
+    GPIO.output(trig, True)
+    time.sleep(0.00001)
+    GPIO.output(trig, False)
+    while GPIO.input(echo) == 0:
+        pulse_start = time.time()
+    while GPIO.input(echo) == 1:
+        pulse_end = time.time()
+    pulse_duration = pulse_end-pulse_start
+    distance = pulse_duration*17000
+    distance = round(distance, 2)
+    return distance
+
+
+def REVERSE(x):
+    if x == True:
+        return False
+    elif x == False:
+        return True
+
+
+forward0 = True
+forward1 = False
+
+backward0 = REVERSE(forward0)
+backward1 = REVERSE(forward1)
+
+MotorLeft_A = 12
+MotorLeft_B = 11
+MotorLeft_PWM = 35
+
+MotorRight_A = 15
+MotorRight_B = 13
+MotorRight_PWM = 37
+
+
+def leftmotor(x):
+    if x == True:
+        GPIO.output(MotorLeft_A, GPIO.HIGH)
+        GPIO.output(MotorLeft_B, GPIO.LOW)
+    elif x == False:
+        GPIO.output(MotorLeft_A, GPIO.LOW)
+        GPIO.output(MotorLeft_B, GPIO.HIGH)
+    else:
+        print
+        'Config Error'
+
+
+def rightmotor(x):
+    if x == True:
+        GPIO.output(MotorRight_A, GPIO.LOW)
+        GPIO.output(MotorRight_B, GPIO.HIGH)
+    elif x == False:
+        GPIO.output(MotorRight_A, GPIO.HIGH)
+        GPIO.output(MotorRight_B, GPIO.LOW)
+
+
+GPIO.setup(MotorLeft_A, GPIO.OUT)
+GPIO.setup(MotorLeft_B, GPIO.OUT)
+GPIO.setup(MotorLeft_PWM, GPIO.OUT)
+
+GPIO.setup(MotorRight_A, GPIO.OUT)
+GPIO.setup(MotorRight_B, GPIO.OUT)
+GPIO.setup(MotorRight_PWM, GPIO.OUT)
+
+LeftPwm = GPIO.PWM(MotorLeft_PWM, 100)
+RightPwm = GPIO.PWM(MotorRight_PWM, 100)
+
+
+def rightSwingTurn(speed, running_time):
+    leftmotor(forward1)
+    GPIO.output(MotorLeft_PWM, GPIO.HIGH)
+    GPIO.output(MotorRight_PWM, GPIO.LOW)
+    LeftPwm.ChangeDutyCycle(speed)
+    RightPwm.ChangeDutyCycle(0)
+    time.sleep(running_time)
+
+
+def leftSwingTurn(speed, running_time):
+    GPIO.output(MotorLeft_PWM, GPIO.LOW)
+    rightmotor(forward0)
+    GPIO.output(MotorRight_PWM, GPIO.HIGH)
+    LeftPwm.ChangeDutyCycle(0)
+    RightPwm.ChangeDutyCycle(speed)
+    time.sleep(running_time)
+
+
+def rightPointTurn(speed, running_time):
+    leftmotor(forward1)
+    GPIO.output(MotorLeft_PWM, GPIO.HIGH)
+    rightmotor(backward0)
+    GPIO.output(MotorRight_PWM, GPIO.HIGH)
+    LeftPwm.ChangeDutyCycle(speed)
+    RightPwm.ChangeDutyCycle(speed)
+    time.sleep(running_time)
+
+
+def leftPointTurn(speed, running_time):
+    rightmotor(forward0)
+    GPIO.output(MotorRight_PWM, GPIO.HIGH)
+    leftmotor(backward1)
+    GPIO.output(MotorLeft_PWM, GPIO.HIGH)
+    LeftPwm.ChangeDutyCycle(speed)
+    RightPwm.ChangeDutyCycle(speed)
+    time.sleep(running_time)
+
+
+def go_forward_any(speed):
+    leftmotor(forward0)
+    leftmotor(forward1)
+    GPIO.output(MotorLeft_PWM, GPIO.HIGH)
+
+    rightmotor(forward0)
+    rightmotor(forward1)
+    GPIO.output(MotorRight_PWM, GPIO.HIGH)
+    LeftPwm.ChangeDutyCycle(speed)
+    RightPwm.ChangeDutyCycle(speed)
+
+
+def go_backward_any(speed):
+    leftmotor(forward0)
+    leftmotor(forward1)
+    GPIO.output(MotorLeft_PWM, GPIO.HIGH)
+    rightmotor(forward0)
+    rightmotor(forward1)
+    GPIO.output(MotorRight_PWM, GPIO.HIGH)
+    LeftPwm.ChangeDutyCycle(speed)
+    RightPwm.ChangeDutyCycle(speed)
+
+
+def go_forward(speed, running_time):
+    leftmotor(forward0)
+    leftmotor(forward1)
+    GPIO.output(MotorLeft_PWM, GPIO.HIGH)
+    rightmotor(forward0)
+    rightmotor(forward1)
+    GPIO.output(MotorRight_PWM, GPIO.HIGH)
+    LeftPwm.ChangeDutyCycle(speed)
+    RightPwm.ChangeDutyCycle(speed)
+    time.sleep(running_time)
+
+
+def go_backward(speed, running_time):
+    rightmotor(backward0)
+    rightmotor(backward1)
+    GPIO.output(MotorRight_PWM, GPIO.HIGH)
+    leftmotor(backward0)
+    leftmotor(backward1)
+    GPIO.output(MotorLeft_PWM, GPIO.HIGH)
+    LeftPwm.ChangeDutyCycle(speed)
+    RightPwm.ChangeDutyCycle(speed)
+    time.sleep(running_time)
+
+
+def stop():
+    GPIO.output(MotorLeft_PWM, GPIO.LOW)
+    GPIO.output(MotorRight_PWM, GPIO.LOW)
+    LeftPwm.ChangeDutyCycle(0)
+    RightPwm.ChangeDutyCycle(0)
+
+
+def pwm_setup():
+    LeftPwm.start(0)
+    RightPwm.start(0)
+
+
+def pwm_low():
+    GPIO.output(MotorLeft_PWM, GPIO.LOW)
+    GPIO.output(MotorRight_PWM, GPIO.LOW)
+    LeftPwm.ChangeDutyCycle(0)
+    RightPwm.ChangeDutyCycle(0)
+    GPIO.cleanup()
 
 # =======================================================================
 # declare the pins of 16, 18, 22, 40, 32 in the Rapberry Pi
@@ -102,7 +273,7 @@ GPIO.setup(rightmostled, GPIO.IN)
 #
 # =======================================================================
 
-
+# we need this part!!!
 try:
     while True:
         print("leftmostled  detects black line(0) or white ground(1): " + str(GPIO.input(leftmostled)))
@@ -110,10 +281,20 @@ try:
         print("centerled    detects black line(0) or white ground(1): " + str(GPIO.input(centerled)))
         print("rightlessled detects black line(0) or white ground(1): " + str(GPIO.input(rightlessled)))
         print("rightmostled detects black line(0) or white ground(1): " + str(GPIO.input(rightmostled)))
-        sleep(1)
+        time.sleep(1)
 
 except KeyboardInterrupt:
+    # the speed of left motor will be set as LOW
+    GPIO.output(MotorLeft_PWM, GPIO.LOW)
+    # left motor will be stopped with function of ChangeDutyCycle(0)
+    LeftPwm.ChangeDutyCycle(0)
+    # the speed of right motor will be set as LOW
+    GPIO.output(MotorRight_PWM, GPIO.LOW)
+    # right motor will be stopped with function of ChangeDutyCycle(0)
+    RightPwm.ChangeDutyCycle(0)
+    # GPIO pin setup has been cleared
     GPIO.cleanup()
+
 
 
 
