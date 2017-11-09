cad715a8eee2882c3ec6eb01c2ce2c3c524f6766
minwook <minwook0106@gmail.com>
2017-11-09 14:59:01 +0900

    add comment, improve code

--- picar-assignment_4/picar-assignment4.py
+++ picar-assignment_4/picar-assignment4.py
@@ -271,6 +271,10 @@ def pwm_low():
 
 
 def linetracing():
+    """
+    Line tracing module
+    0 = white, 1 = black
+    """
     a = int(GPIO.input(leftmostled))
     b = int(GPIO.input(leftlessled))
     c = int(GPIO.input(centerled))
@@ -342,41 +346,7 @@ def linetracing():
         go_forward_any(0, 40)
 
 
-
-# =======================================================================
-# declare the pins of 16, 18, 22, 40, 32 in the Rapberry Pi
-# as the control pins of 5-way trackinmg sensor in order to
-# control direction
-#
-#  leftmostled    leftlessled     centerled     rightlessled     rightmostled
-#       16            18              22             40              32
-#
-# led turns on (1) : trackinmg sensor led detects white playground
-# led turns off(0) : trackinmg sensor led detects black line
-
-# leftmostled off : it means that moving object finds black line
-#                   at the position of leftmostled
-#                   black line locates below the leftmostled of the moving object
-#
-# leftlessled off : it means that moving object finds black line
-#                   at the position of leftlessled
-#                   black line locates below the leftlessled of the moving object
-#
-# centerled off : it means that moving object finds black line
-#                   at the position of centerled
-#                   black line locates below the centerled of the moving object
-#
-# rightlessled off : it means that moving object finds black line
-#                   at the position of rightlessled
-#                   black line locates below the rightlessled  of the moving object
-#
-# rightmostled off : it means that moving object finds black line
-#                   at the position of rightmostled
-#                   black line locates below the rightmostled of the moving object
-# =======================================================================
-
-
-# declare the pins of 16, 18, 22, 40, 32 in the Rapberry Pi
+# 5-way tracking sensor's pin number
 dis = 15
 leftmostled = 16
 leftlessled = 18
@@ -384,49 +354,14 @@ centerled = 22
 rightlessled = 40
 rightmostled = 32
 
-# =======================================================================
-# because the connetions between 5-way tracking sensor and Rapberry Pi has been
-# established, the GPIO pins of Rapberry Pi
-# such as leftmostled, leftlessled, centerled, rightlessled, and rightmostled
-# should be clearly declared whether their roles of pins
-# are output pin or input pin
-# since the 5-way tracking sensor data has been detected and
-# used as the input data, leftmostled, leftlessled, centerled, rightlessled, and rightmostled
-# should be clearly declared as input
-#
-# =======================================================================
-
+# GPIO input setting
 GPIO.setup(leftmostled, GPIO.IN)
 GPIO.setup(leftlessled, GPIO.IN)
 GPIO.setup(centerled, GPIO.IN)
 GPIO.setup(rightlessled, GPIO.IN)
 GPIO.setup(rightmostled, GPIO.IN)
 
-# =======================================================================
-# GPIO.input(leftmostled) method gives the data obtained from leftmostled
-# leftmostled returns (1) : leftmostled detects white playground
-# leftmostled returns (0) : leftmostled detects black line
-#
-#
-# GPIO.input(leftlessled) method gives the data obtained from leftlessled
-# leftlessled returns (1) : leftlessled detects white playground
-# leftlessled returns (0) : leftlessled detects black line
-#
-# GPIO.input(centerled) method gives the data obtained from centerled
-# centerled returns (1) : centerled detects white playground
-# centerled returns (0) : centerled detects black line
-#
-# GPIO.input(rightlessled) method gives the data obtained from rightlessled
-# rightlessled returns (1) : rightlessled detects white playground
-# rightlessled returns (0) : rightlessled detects black line
-#
-# GPIO.input(rightmostled) method gives the data obtained from rightmostled
-# rightmostled returns (1) : rightmostled detects white playground
-# rightmostled returns (0) : rightmostled detects black line
-#
-# =======================================================================
-
-# we need this part!!!
+# Main
 try:
     while True:
         print("leftmostled  detects black line(0) or white ground(1): " + str(GPIO.input(leftmostled)))
@@ -443,14 +378,13 @@ try:
 
         # when the distance is below the dis, moving object stops
         else:
-            # stop and wait 1 second
             stop()
             time.sleep(1)
             rightPointTurn(37, 0.38)
             time.sleep(1)
             go_forward(38, 0.3)
             time.sleep(1)
-            while((int(GPIO.input(leftmostled)) == 1) & (int(GPIO.input(leftlessled)) == 1) & (int(GPIO.input(centerled)) == 1) & (int(GPIO.input(rightmostled)) == 1) & (int(GPIO.input(rightlessled)) == 1)):
+            while (int(GPIO.input(leftmostled)) == 1) & (int(GPIO.input(leftlessled)) == 1) & (int(GPIO.input(centerled)) == 1) & (int(GPIO.input(rightmostled)) == 1) & (int(GPIO.input(rightlessled)) == 1):
                 leftPointTurn(33, 0.35)
                 time.sleep(1)
                 go_forward(42, 0.7)
