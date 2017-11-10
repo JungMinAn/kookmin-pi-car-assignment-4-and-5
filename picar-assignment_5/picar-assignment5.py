e081cd3444456cc1d0bcda0bf89d1dab96a18a94
JungMin An <qqqwkd@kookmin.ac.kr>
2017-11-10 15:08:20 +0900

    delete getdistance func

--- picar-assignment_5/picar-assignment5.py
+++ picar-assignment_5/picar-assignment5.py
@@ -21,27 +21,6 @@ echo = 31
 GPIO.setup(trig, GPIO.OUT)
 GPIO.setup(echo, GPIO.IN)
 
-
-def getDistance():
-    """
-    Detect distance
-    :return: distance
-    """
-    GPIO.output(trig, False)
-    time.sleep(0.5)
-    GPIO.output(trig, True)
-    time.sleep(0.00001)
-    GPIO.output(trig, False)
-    while GPIO.input(echo) == 0:
-        pulse_start = time.time()
-    while GPIO.input(echo) == 1:
-        pulse_end = time.time()
-    pulse_duration = pulse_end-pulse_start
-    distance = pulse_duration*17000
-    distance = round(distance, 2)
-    return distance
-
-
 def REVERSE(x):
     """
     Reverse for forward
